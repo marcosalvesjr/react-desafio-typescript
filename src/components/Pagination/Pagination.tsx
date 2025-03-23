90068be8a6ea401398e3503ac06706708cff6ba5
Marcos Alves <mrcaurelio95@gmail.com>
2025-03-23 08:33:49 -0300

    Style: estilizando pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -21,14 +21,24 @@ const Pagination: React.FC<Props> = ({ info, pageNumber, setPageNumber }) => {
         onPageChange={(data) => {
           setPageNumber(data.selected + 1);
           window.scroll(0, 0);
+          console.log(pageNumber);
         }}
         nextLabel="Próxima >"
-        nextLinkClassName="font-bold hidden sm:block"
+        nextLinkClassName={`font-bold hidden sm:block  ${
+          pageNumber === 42
+            ? "font-light"
+            : "cursor-pointer hover:text-orange-500"
+        }`}
         previousLabel="< Anterior"
-        previousLinkClassName="font-bold hidden sm:block"
+        previousLinkClassName={`font-bold hidden sm:block  ${
+          pageNumber === 1
+            ? "font-light"
+            : "cursor-pointer hover:text-orange-500"
+        } `}
         containerClassName=" flex bg-slate-300 gap-4 p-2 m-2 rounded-lg"
         forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
         activeClassName="text-orange-500 font-bold"
+        pageLinkClassName="cursor-pointer hover:font-black"
       />
     </div>
   );
