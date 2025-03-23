154645ec162cf3a49435b0c1be1c806432ccdcd3
Marcos Alves <mrcaurelio95@gmail.com>
2025-03-23 08:15:57 -0300

    feat: Atualizando paginação
    Usando react-paginate para fazer paginação

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,3 +1,5 @@
+import ReactPaginate from "react-paginate";
+
 type InfoType = {
   count: number; // Total de itens
   pages: number; // Total de páginas
@@ -12,31 +14,22 @@ type Props = {
 };
 
 const Pagination: React.FC<Props> = ({ info, pageNumber, setPageNumber }) => {
-  const prevPage = () => {
-    setPageNumber(pageNumber - 1);
-    window.scrollTo(0, 0);
-  };
-  const nextPage = () => {
-    setPageNumber(pageNumber + 1);
-    window.scrollTo(0, 0);
-  };
   return (
-    <div className="flex gap-4 justify-center">
-      <button
-        disabled={pageNumber === 1}
-        onClick={prevPage}
-        className={`${pageNumber === 1 ? "text-red-500" : "text-amber-500"}`}
-      >
-        Voltar
-      </button>
-      <button>{pageNumber}</button>
-      <button
-        className={`${pageNumber === 42 ? "text-red-500" : "text-amber-500"}`}
-        disabled={pageNumber === 42}
-        onClick={nextPage}
-      >
-        Proxima
-      </button>
+    <div>
+      <ReactPaginate
+        pageCount={info.pages}
+        onPageChange={(data) => {
+          setPageNumber(data.selected + 1);
+          window.scroll(0, 0);
+        }}
+        nextLabel="Próxima >"
+        nextLinkClassName="font-bold hidden sm:block"
+        previousLabel="< Anterior"
+        previousLinkClassName="font-bold hidden sm:block"
+        containerClassName=" flex bg-slate-300 gap-4 p-2 m-2 rounded-lg"
+        forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
+        activeClassName="text-orange-500 font-bold"
+      />
     </div>
   );
 };
